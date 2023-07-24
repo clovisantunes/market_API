2cbe301cb60d848b4fb449599d8b232307034ec5
Clovis antunes <clovissantannaa@gmail.com>
2023-07-24 09:08:53 -0300

    Added cors

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -14,7 +14,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 - Adjusted README.md
 - Create routes 
 - Create middlewares error 500
-
+- Added cors
 
 ### Fixed
 
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response, NextFunction} from 'express';
 import 'express-async-errors';
 import { router } from './routes';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
-
+app.use(cors())
 app.use(router);
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) =>{
